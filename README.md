eda9eaba038534cc0bce338340d1dee5e939a595
farinazo <51508868+farinazo@users.noreply.github.com>
2024-12-06 02:35:42 +0000

    adiciona os scripts 'lint:check' e 'lint:fix'

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Este é um repositório criado para que eu possa aprender a implementar o https://www.tabnews.com.br com o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>O PS5 chegou! Aeeeeeeeee! 🎮</h1>
+  return <h1>O PS5 chegou! Aeeeeeeeee! 🎮</h1>;
 }
 
 export default Home;
